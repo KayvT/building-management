bdaf582d03bbbe73637aa752240378d96a1d529b
KayvT <Khaled.yassen@outlook.com>
2025-01-10 01:44:38 +0300

    feat(query): add query for getting topology of tenant

--- client/src/graphql/queries/tenants.ts
+++ client/src/graphql/queries/tenants.ts
@@ -8,3 +8,24 @@ export const GET_ALL_TENANTS = gql`
     }
   }
 `;
+
+export const GET_TOPOLOGY = gql`
+  query GetTopology {
+    tenant {
+      id
+      name
+      floors {
+        id
+        name
+        locations {
+          id
+          name
+          spots {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
