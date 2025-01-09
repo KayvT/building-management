5b508c5cadbf72571159012d2645f4b51e64a269
KayvT <Khaled.yassen@outlook.com>
2025-01-10 00:42:12 +0300

    feat(navigation): remove comments

--- client/src/components/AppWrapper.tsx
+++ client/src/components/AppWrapper.tsx
@@ -11,17 +11,11 @@ export default function AppWrapper({ children }: { children: ReactNode }) {
   const { currentTenantId, setCurrentTenantId } = useTenant();
 
   useEffect(() => {
-    // If we have a URL tenant ID, use that
     if (tenantId) {
       setCurrentTenantId(tenantId);
-    } 
-    // If we don't have a URL tenant ID but have one in context/localStorage, keep using that
-    else if (currentTenantId) {
-      // No need to setCurrentTenantId since we already have it
+    } else if (currentTenantId) {
       return;
-    } 
-    // If we have neither, use the first tenant from the query
-    else if (data?.tenants?.[0]?.id) {
+    } else if (data?.tenants?.[0]?.id) {
       setCurrentTenantId(data.tenants[0].id);
     }
   }, [tenantId, currentTenantId, setCurrentTenantId, data]);
