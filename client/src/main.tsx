083778b28d1cbbcd41ed5a1c649f91361d87438c
KayvT <Khaled.yassen@outlook.com>
2025-01-11 18:40:57 +0300

    feat(app): add background

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <div className="h-screen w-screen bg-gray-100">
+      <div className="h-screen w-screen absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
         <App />
       </div>
     </BrowserRouter>
