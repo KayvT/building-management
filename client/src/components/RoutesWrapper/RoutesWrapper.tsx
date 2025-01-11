b256b46612caf0036b4e8867eff47997a716c204
KayvT <Khaled.yassen@outlook.com>
2025-01-11 16:44:30 +0300

    feat(app): add sub routes to migrate app to use URL for branches

--- client/src/components/RoutesWrapper/RoutesWrapper.tsx
+++ client/src/components/RoutesWrapper/RoutesWrapper.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-
-
-import { Route, Routes} from "react-router";
-import TopologyRoute from "../../routes/topology/route";
+import { Route, Routes } from "react-router";
+import TopologyLayout from "../../routes/topology/TopologyLayout";
 import TasksRoute from "../../routes/tasks/route";
 import OperativesRoute from "../../routes/operatives/route";
+import { FloorView } from "../TopologyContent/FloorView";
+import { LocationView } from "../TopologyContent/LocationView";
 
 export default function RoutesWrapper() {
   return (
     <Routes>
-      <Route path="/:tenantId/topology" element={<TopologyRoute />} />
+      <Route path="/:tenantId/topology" element={<TopologyLayout />}>
+        <Route path="floors/:floorId" element={<FloorView />} />
+        <Route
+          path="floors/:floorId/locations/:locationId"
+          element={<LocationView />}
+        />
+        <Route
+          path="floors/:floorId/locations/:locationId/spots/:spotId"
+          element={<div>Spot View</div>}
+        />
+      </Route>
       <Route path="/:tenantId/tasks" element={<TasksRoute />} />
       <Route path="/:tenantId/operatives" element={<OperativesRoute />} />
     </Routes>
