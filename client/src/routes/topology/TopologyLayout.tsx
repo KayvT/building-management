c1d809fbf10baa2329a3dca5a6510c1ce1d3f5fb
KayvT <Khaled.yassen@outlook.com>
2025-01-12 00:37:08 +0300

    chore(tasks): style fixes

--- client/src/routes/topology/TopologyLayout.tsx
+++ client/src/routes/topology/TopologyLayout.tsx
@@ -16,44 +16,67 @@ export default function TopologyLayout() {
     <div
       style={{
         display: "flex",
-        maxWidth: "80%",
         height: "calc(100vh - 250px)",
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: "start",
+        alignItems: "start",
         margin: "48px auto",
+        flexDirection: "column",
       }}
     >
       <div
+        className="flex flex-row justify-between items-center w-full"
         style={{
-          padding: "1rem",
+          width: "100%",
+          maxWidth: "1100px",
+          margin: "auto",
+          paddingLeft: "2rem",
         }}
       >
-        {topologyTreeData && <TopologyTreeView tree={topologyTreeData} />}
+        <h1 className="text-5xl font-bold">Topology Tree</h1>
       </div>
-      <Box
-        className="flex flex-col"
-        sx={{
+      <div
+        style={{
+          display: "flex",
           flex: 1,
-          maxWidth: "800px",
-          boxShadow: "0 0 16px 0 rgba(0, 0, 0, 0.1)",
-          backgroundColor: "white",
-          borderRadius: "8px",
-          height: "800px",
-          overflowY: "auto",
-          padding: "1rem 2rem",
+          width: "100%",
+          height: "calc(100vh - 250px)",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto auto",
         }}
-        boxShadow={4}
       >
-        {isEmptyView ? (
-          <div className="flex flex-row gap-2 items-center justify-center h-full">
-            <p className="text-xl text-gray-500 opacity-50">
-              Please select an entry to view its details.
-            </p>
-          </div>
-        ) : (
-          <Outlet />
-        )}
-      </Box>
+        <div
+          style={{
+            padding: "1rem",
+          }}
+        >
+          {topologyTreeData && <TopologyTreeView tree={topologyTreeData} />}
+        </div>
+        <Box
+          className="flex flex-col"
+          sx={{
+            flex: 1,
+            maxWidth: "800px",
+            boxShadow: "0 0 16px 0 rgba(0, 0, 0, 0.1)",
+            backgroundColor: "white",
+            borderRadius: "8px",
+            height: "800px",
+            overflowY: "auto",
+            padding: "1rem 2rem",
+          }}
+          boxShadow={4}
+        >
+          {isEmptyView ? (
+            <div className="flex flex-row gap-2 items-center justify-center h-full">
+              <p className="text-xl text-gray-500 opacity-50">
+                Please select an entry to view its details.
+              </p>
+            </div>
+          ) : (
+            <Outlet />
+          )}
+        </Box>
+      </div>
     </div>
   );
 }
