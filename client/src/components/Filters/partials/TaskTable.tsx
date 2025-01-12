53345cb975fbe1b6839c6bea50884b061782188f
KayvT <Khaled.yassen@outlook.com>
2025-01-12 18:42:41 +0300

    chore(app): refactor task table

--- client/src/components/Filters/partials/TaskTable.tsx
+++ client/src/components/Filters/partials/TaskTable.tsx
@@ -1,7 +1,7 @@
 import { Task } from "@/types/tasks";
 import { Tasks } from "@/types/tasks";
 import { useNavigate, useParams } from "react-router-dom";
-
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 
 type TaskTableProps = {
   tasks: Tasks;
@@ -16,6 +16,34 @@ export default function TaskTable({ tasks }: TaskTableProps) {
     navigate(`/${tenantId}/tasks/${task.id}`);
   };
 
+  const getTaskPriorityTag = (priority: string) => {
+    switch (priority) {
+      case "LOW":
+        return (
+          <span className="bg-green-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+            {priority}
+          </span>
+        );
+      case "MEDIUM":
+        return (
+          <span className="bg-yellow-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+            {priority}
+          </span>
+        );
+      case "URGENT":
+        return (
+          <span className="bg-red-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+            {priority}
+          </span>
+        );
+      default:
+        return (
+          <span className="bg-purple-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+            {priority}
+          </span>
+        );
+    }
+  };
   return (
     <div className="flex flex-row justify-between items-center w-full">
       <table className="w-full border-collapse">
@@ -31,70 +59,64 @@ export default function TaskTable({ tasks }: TaskTableProps) {
           </tr>
         </thead>
         <tbody>
-          {tasks.map((task) => (
-            <tr
-              key={task.id}
-              onClick={() => handleTaskClick(task)}
-              className="border-b hover:bg-gray-200 transition-all duration-300 cursor-pointer"
-            >
-              <td className="p-3 text-center">{task?.id}</td>
-              <td className="p-3 text-center">{task?.location?.name}</td>
-              <td className="p-3 text-center">
-                {task?.priority === "LOW" ? (
-                  <span className="bg-green-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
-                    {task?.priority}
-                  </span>
-                ) : task?.priority === "MEDIUM" ? (
-                  <span className="bg-yellow-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
-                    {task?.priority}
-                  </span>
-                ) : task?.priority === "URGENT" ? (
-                  <span className="bg-red-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
-                    {task?.priority}
-                  </span>
-                ) : (
-                  <span className="bg-purple-500 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
-                    {task?.priority}
-                  </span>
-                )}
-              </td>
-              <td className="p-3 text-center">
-                {new Date(task?.createdAt).toLocaleString()}
-              </td>
-              <td className="p-3 text-center">
-                {new Date(task?.dueAt).toLocaleString()}
-              </td>
-              <td className="p-3 text-center">
-                {task?.state === "OPEN" ? (
-                  <span className="bg-[#ECFBF3] text-[#254C3A] text-xs font-medium me-2 px-2.5 py-0.5 rounded-full border border-[#254C3A]">
-                    Open
-                  </span>
-                ) : (
-                  <span className="bg-[#FFF0EC] text-[#5F1908] text-xs font-medium me-2 px-2.5 py-0.5 rounded-full border border-[#5F1908]">
-                    CLOSED
-                  </span>
-                )}
-              </td>
-              <td className="p-3">
-                <div className="flex items-center justify-center">
-                  {task?.operative?.id ? (
-                    <div
-                      className="w-10 h-10 rounded-full flex items-center justify-center"
-                      style={{
-                        backgroundColor: "#755596",
-                      }}
-                    >
-                      <div className="text-white font-bold">
-                        {task?.operative?.name?.charAt(0).toUpperCase()}
-                      </div>
-                    </div>
+          {tasks.length > 0 ? (
+            tasks.map((task) => (
+              <tr
+                key={task.id}
+                onClick={() => handleTaskClick(task)}
+                className="border-b hover:bg-gray-200 transition-all duration-300 cursor-pointer"
+              >
+                <td className="p-3 text-center text-gray-500">{task?.id}</td>
+                <td className="p-3 text-center">{task?.location?.name}</td>
+                <td className="p-3 text-center">
+                  {getTaskPriorityTag(task?.priority)}
+                </td>
+                <td className="p-3 text-center">
+                  {new Date(task?.createdAt).toLocaleString()}
+                </td>
+                <td className="p-3 text-center">
+                  {new Date(task?.dueAt).toLocaleString()}
+                </td>
+                <td className="p-3 text-center">
+                  {task?.state === "OPEN" ? (
+                    <span className="bg-[#ECFBF3] text-[#254C3A] text-xs font-medium me-2 px-2.5 py-0.5 rounded-full border border-[#254C3A]">
+                      Open
+                    </span>
                   ) : (
-                    <span className="text-gray-400">N/A</span>
+                    <span className="bg-[#FFF0EC] text-[#5F1908] text-xs font-medium me-2 px-2.5 py-0.5 rounded-full border border-[#5F1908]">
+                      CLOSED
+                    </span>
                   )}
-                </div>
+                </td>
+                <td className="p-3">
+                  <div className="flex items-center justify-center">
+                    {task?.operative?.id ? (
+                      <div
+                        className="w-10 h-10 rounded-full flex items-center justify-center"
+                        style={{
+                          backgroundColor: "#755596",
+                        }}
+                      >
+                        <div className="text-white font-bold">
+                          {task?.operative?.name?.charAt(0).toUpperCase()}
+                        </div>
+                      </div>
+                    ) : (
+                      <span className="text-gray-400">
+                        <PersonAddAltIcon />
+                      </span>
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={7} className="p-3 text-center">
+                No tasks found
               </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
