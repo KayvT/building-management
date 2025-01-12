dea2a43f60f3f1fedd3293d5d322f4f6df36d1f8
KayvT <Khaled.yassen@outlook.com>
2025-01-12 19:59:19 +0300

    chore(app): update link for be

--- client/src/contexts/ApolloClientProvider/TenantApolloProvider.tsx
+++ client/src/contexts/ApolloClientProvider/TenantApolloProvider.tsx
@@ -11,7 +11,7 @@ export function TenantApolloProvider({
   const { currentTenantId } = useTenant();
   const client = useMemo(() => {
     return new ApolloClient({
-      uri: "http://localhost:4000",
+      uri: "https://building-management-8ed5.onrender.com",
       cache: new InMemoryCache(),
       headers: {
         "X-Tenant-ID": currentTenantId || "",
