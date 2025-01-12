b4f35e6302992c09d005e3ce1f24750ca0bde45a
KayvT <Khaled.yassen@outlook.com>
2025-01-12 13:13:20 +0300

    feat(app): add queries for tasks

--- client/src/graphql/queries/tasks.ts
+++ client/src/graphql/queries/tasks.ts
@@ -19,3 +19,39 @@ export const GET_TASKS = gql`
     }
   }
 `;
+
+export const GET_TASK = gql`
+  query GetTask($id: ID!) {
+    task(id: $id) {
+      id
+      location {
+        name
+        id
+      }
+      state
+      createdAt
+      dueAt
+      priority
+      operative {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const ASSIGN_TASK = gql`
+  mutation AssignTask($taskId: ID!, $operativeId: ID) {
+    assignTask(taskId: $taskId, operativeId: $operativeId) {
+      id
+    }
+  }
+`;
+
+export const CLOSE_TASK = gql`
+  mutation CloseTask($taskId: ID!) {
+    closeTask(taskId: $taskId) {
+      id
+    }
+  }
+`;
