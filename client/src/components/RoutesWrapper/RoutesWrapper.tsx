134fea4c7df970eb54dbf3a3d409c8f27e2af317
KayvT <Khaled.yassen@outlook.com>
2025-01-12 20:56:40 +0300

    chore(app): match base route to go to topology

--- client/src/components/RoutesWrapper/RoutesWrapper.tsx
+++ client/src/components/RoutesWrapper/RoutesWrapper.tsx
@@ -1,14 +1,27 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 import TopologyLayout from "../../routes/topology/TopologyLayout";
 import OperativesRoute from "../../routes/operatives/route";
 import { FloorView } from "../TopologyContent/FloorView";
 import { LocationView } from "../TopologyContent/LocationView";
 import TasksLayout from "../../routes/tasks/TasksLayout";
 import { TaskDetailView } from "../TaskDetailView/TaskDetailView";
+import { useTenant } from "../../contexts/TenantIdContext/useTenant";
 
 export default function RoutesWrapper() {
+  const { currentTenantId } = useTenant();
+
   return (
     <Routes>
+      <Route index element={
+        currentTenantId ? 
+        <Navigate to={`/${currentTenantId}/topology`} replace /> : 
+        null
+      } />
+      <Route path="/" element={
+        currentTenantId ? 
+        <Navigate to={`/${currentTenantId}/topology`} replace /> : 
+        null
+      } />
       <Route path="/:tenantId/topology" element={<TopologyLayout />}>
         <Route path="floors/:floorId" element={<FloorView />} />
         <Route
